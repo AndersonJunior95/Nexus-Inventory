4c7645cf52f82cce335ea143999241a2b9119b0d
AndersonJunior95 <andersongoncalves8419@gmail.com>
2026-01-21 21:16:52 -0300

    feat: Adicionar páginas de registro e login com respectivos estilos e scripts
    Criação das páginas register.html e index.html para os processos de cadastro e autenticação de usuários.
    
    Inclusão de arquivos CSS para a estilização visual das interfaces de registro e login.
    
    Implementação de lógica em JavaScript para o gerenciamento da funcionalidade de login e renderização de dados no dashboard.
    
    Desenvolvimento da página dashboard.html voltada à exibição do painel do usuário com rastreamento de atividades.
    
    Introdução de configurações de tema com suporte ao modo escuro (dark mode).
    
    Adição de funcionalidade para upload de avatar no formulário de cadastro.
    
    Implementação do gerenciamento de sessão de usuário utilizando localStorage.

---
+++ public/js/dashboard.js
@@ -0,0 +1,165 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Mockando os dados
+  const dashboardData = {
+    kpis: {
+      totalAssets: 1240,
+      inUse: 1054,
+      maintenance: 45,
+      activeUsers: 850,
+    },
+
+    recentActivities: [
+      {
+        user: "Ana Silva",
+        action: "Retirou",
+        device: "MacBook Pro M2",
+        time: "Há 2 minutos",
+        status: "Saída",
+        statusColor: "blue",
+      },
+    ],
+  };
+
+  /* Seletores */
+
+  const kpiTotalElements = document.getElementById("kpi-total-assets");
+  const kpiInUseElements = document.getElementById("kpi-in-use");
+  const kpiMaintenanceElements = document.getElementById("kpi-maintenance");
+  const kpiUsersElements = document.getElementById("kpi-users");
+  const activityListElement = document.getElementById("activity-list");
+  const searchInput = document.getElementById("search-input");
+  const logoutBtn = document.querySelector(".logout-btn");
+  const checkoutBtn = document.querySelector(".action-card.primary");
+  const actionCards = document.querySelectorAll(".action-card:not(.primary):not(.dashed)");
+  const rawData = localStorage.getItem('nexus_user');
+  const user = JSON.parse(rawData);
+  const welcomeName = document.querySelector(".welcome-text h2");
+  const sidebarName = document.querySelector(".user-info strong");
+  const currentHour = new Date().getHours();
+
+  /* Renderização das tabelas */
+
+  kpiInUseElements.textContent = dashboardData.kpis.totalAssets;
+  kpiInUseElements.textContent = dashboardData.kpis.inUse;
+  kpiMaintenanceElements.textContent = dashboardData.kpis.maintenance;
+  kpiUsersElements.textContent = dashboardData.kpis.activeUsers;
+  kpiTotalElements.textContent = dashboardData.kpis.totalAssets;
+
+  /* Horário Manha, Tarde e Noite */
+
+  function getHoursDay(hour) {
+    if (hour >= 5 && hour < 12) {
+      return "Bom dia";
+    }
+    if (hour >= 12 && hour < 18) {
+      return "Boa tarde";
+    }
+    return "Boa noite";
+  }
+  /* Renderizando user */
+
+  if (!rawData) {
+    window.location.href = "./index.html";
+    return;
+  }
+
+  if (welcomeName) {
+    const greeting = getHoursDay(currentHour);
+    welcomeName.innerHTML = `${greeting}, ${user.user}!`;
+  }
+  if (sidebarName) sidebarName.textContent = user.user;
+
+
+
+  /* Utilizando o MAP */
+
+  function renderActivities(listItems) {
+    activityListElement.innerHTML = "";
+
+    if (listItems.length === 0) {
+      activityListElement.innerHTML =
+        '<div class="no-result">Nenhuma atividade encontrada.</div>';
+      return;
+    }
+
+    const listHTML = listItems
+      .map((activity) => {
+        return `
+            <div class="activity-item">
+                <div class="avatar-placeholder soft-purple">
+                    ${activity.user.charAt(0)}
+                </div>
+                <div class="activity-details">
+                    <p>
+                        <strong> ${activity.user} </strong>
+                         ${activity.action}
+                        <strong> ${activity.device}</strong>
+                    </p>
+                </div>
+                <span class="status-pill ${activity.statusColor}">
+                    ${activity.status}
+                </span>
+            </div>   
+        `;
+      })
+      .join("");
+
+    activityListElement.innerHTML = listHTML;
+  }
+  renderActivities(dashboardData.recentActivities);
+
+  /* Filtro */
+
+  searchInput.addEventListener("input", (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+
+    const filteredActivities = dashboardData.recentActivities.filter((activity) => {
+      return activity.user.toLowerCase().includes(searchTerm) ||
+        activity.device.toLowerCase().includes(searchTerm);
+    });
+    renderActivities(filteredActivities);
+  });
+
+  /* Logout */
+  if (logoutBtn) {
+    logoutBtn.addEventListener("click", () => {
+      // Depois farei o logout com backend
+      console.log("Encerrando a sessão...");
+      window.location.href = "./index.html";
+    })
+  }
+
+  /* Ações rápidas */
+
+  if (checkoutBtn) {
+    checkoutBtn.addEventListener("click", () => {
+      const assetTag = prompt("Digite a TAG do ativo que deseja retirar:");
+      if (assetTag) {
+
+        alert(`Ativo com TAG ${assetTag} retirado com sucesso!`);
+        // Depois vou chamar a API para registrar a retirada
+      } else {
+        alert("Operação cancelada ou TAG inválida.");
+      };
+    })
+  }
+
+  /* Outras Ações */
+
+  actionCards.forEach((card) => {
+    card.addEventListener("click", () => {
+      const acao = card.querySelector("strong").textContent
+
+      const originalBg = card.style.backgroundColor;
+      card.style.backgroundColor = "rgba(42, 109, 116, 0.05)";
+
+      console.log("Ação selecionada:", acao);
+
+      setTimeout(() => {
+        card.style.backgroundColor = originalBg;
+        alert(`A funcionalidade de ${acao} será implementada na fase de Backend!`);
+      }, 200);
+    });
+  });
+
+});
---
+++ public/js/login.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.getElementById("login-form");
+  const passwordInput = document.getElementById("password");
+  const toggleBtn = document.getElementById("togglePassword");
+
+  if (loginForm) {
+    loginForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const email = document.getElementById("email").value.trim();
+      const password = document.getElementById("password").value.trim();
+
+      console.log("Email digitado: ", email);
+
+      if (!email || !password) {
+        alert("Por favor, preencha todas as informações");
+        return;
+      }
+
+      /* Simulando alguns dados */
+
+      const user = {
+        user: "Sarah Jenkins",
+        role: "Admin",
+        email: email
+      };
+
+      localStorage.setItem('nexus_user', JSON.stringify(user));
+
+      window.location.href = "dashboard.html";
+    });
+
+    if (toggleBtn && passwordInput) {
+      toggleBtn.addEventListener("click", () => {
+        const tipoAtual = passwordInput.getAttribute("type");
+        const iconSpan = toggleBtn.querySelector("span");
+
+        if (tipoAtual === "password") {
+          passwordInput.setAttribute("type", "text");
+          iconSpan.textContent = "visibility_off";
+        } else {
+          passwordInput.setAttribute("type", "password");
+          iconSpan.textContent = "visibility";
+        }
+      });
+    }
+  }
+});
